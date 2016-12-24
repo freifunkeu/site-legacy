72ee826e4bd0810d55e9b2bfcda0876e247efe1d
freifunkeu <info@freifunk-euskirchen.de>
2016-12-24 14:27:16 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
     iptables \
     haveged
 
-DEFAULT_GLUON_RELEASE := 2016.2-0.9.0+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2016.2.2-0.9.0+exp$(shell date '+%Y%m%d')
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 # Languages to include
