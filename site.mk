c75cc7e65406610bc26a531d6983c2a9ac3d51bd
Kevin <k3v1n@k3v1n.de>
2019-07-30 08:11:30 +0200

    Some updates

--- site.mk
+++ site.mk
@@ -5,48 +5,26 @@
 #		Gluon will automatically enable a set of packages
 #		depending on the combination of features listed
 
-## GLUON_FEATURES := \
-#	autoupdater \
-#	ebtables-filter-multicast \
-#	ebtables-filter-ra-dhcp \
-#	mesh-batman-adv-15 \
-#	mesh-vpn-fastd \
-#	radvd \
-#	respondd \
-#	status-page \
-#	web-advanced \
-#	web-wizard
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-15 \
+	mesh-vpn-fastd \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
 
 ##	GLUON_SITE_PACKAGES
-#		Specify additional Gluon/LEDE packages to include here;
+#		Specify additional Gluon/OpenWrt packages to include here;
 #		A minus sign may be prepended to remove a packages from the
 #		selection that would be enabled by default or due to the
 #		chosen feature flags
 
-GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-15 \
-        gluon-respondd \
-        gluon-autoupdater \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-contact-info \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-hostname \
-        gluon-config-mode-core \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-limit-arp \
-        gluon-radv-filterd \
-        respondd-module-airtime \
-        gluon-web-admin \
-        gluon-web-autoupdater \
-        gluon-web-wifi-config \
-        gluon-web-network \
-        gluon-status-page \
-        haveged \
-        iptables \
-        iwinfo \
-        gluon-web-private-wifi \
-        gluon-config-mode-mesh-vpn \
-        gluon-mesh-vpn-fastd
+GLUON_SITE_PACKAGES := haveged iwinfo
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
@@ -54,7 +32,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := fastd-2018.1.4-0.9.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
 
 # Variables set with ?= can be overwritten from the command line
 
